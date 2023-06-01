73b2e2d4e3332b65b3692dc56046802dfda1296a
SHAMI_TOMITA <shamitomita@gmail.com>
2023-06-01 09:28:31 -0400

    Week Crud actions

--- app/controllers/weeks_controller.rb
+++ app/controllers/weeks_controller.rb
@@ -1,2 +1,41 @@
 class WeeksController < ApplicationController
+    before_action :current_week, only: [:show, :edit, :update]
+
+    def index
+        @weeks = Week.all 
+    end
+
+    def new 
+        @week = Week.new
+    end 
+
+    def create
+        if @week.save? 
+           redirect_to week_path(@week), notice: "Created!"
+        else 
+            render :edit 
+        end 
+    end 
+
+    def show 
+    end 
+
+    def update
+        @week.update(week_params)
+
+        redirect_to week_path(@week), notice: "Updated"
+    end 
+
+    def destroy 
+    end 
+
+    private 
+
+    def week_params 
+        params.require(:week).permit(:range)
+    end 
+
+    def current_week
+        @week = Week.find(params[:id]) 
+    end 
 end
